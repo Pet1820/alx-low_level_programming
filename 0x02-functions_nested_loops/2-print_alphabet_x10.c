96844e59489159618106efde9f5323cb1fd6bd17
pet1820 <15peterkinyanjui@gmail.com>
2022-10-18 12:36:14 -0700

    small alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include 'main.h'
+/**
+*print_alphabet-print alphabets ato z in small case
+*/
+void print_alphabet(void);
+char a = 'a';
+{
+	for (a = a; a < z; a++)
+	{
+	putchar("%c\n", a);
+	 ++;
+	}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN.H
 #define MAIN.H
 
-int putchar(char c)
+int putchar(char c);
+void print_alphabet(void);
 
 #endif
