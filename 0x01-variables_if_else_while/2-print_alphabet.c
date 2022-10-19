6a5e44b193fade63cadd2558ba09ff7ca47a9b76
pet1820 <15peterkinyanjui@gmail.com>
2022-10-19 03:35:10 -0700

    prints alphabets in small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*main-prints small alphabetical letters*/
+/*return: 0;*/
+char main(void)
+{
+alph = a;
+while (alph < = z)
+alph++;
+{
+putchar ("%c", alph)
+}
+putchar ("\n")
+return 0
+}
