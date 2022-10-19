69c1796fe4406d14130f31a622c4ac154536b4f3
pet1820 <15peterkinyanjui@gmail.com>
2022-10-19 13:36:27 -0700

    print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*main- 7-print_tebahpla.c*/
+/*return: 0*/
+main(void)
+{
+	counter = z;
+	while (counter >= a)
+	{
+		putchar("%c", counter);
+	}
+	putchar("\n");
+		return (0);
+}
+
