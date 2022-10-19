a82c788e41a6fc8c3ff2590a8bf818355d0fffaf
pet1820 <15peterkinyanjui@gmail.com>
2022-10-19 04:01:44 -0700

    prints alphabet in lowercase less q and e

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*main-prints alphabets excepts q and e*/
+/*return: 0;*/
+
+main(void)
+{
+	alph = a;
+	while (alph < = z)
+	{
+		while (alph != q && alph != e)
+		{
+		putchar ("%c," alph);
+		}
+		putchar ("\n");
+	}
+return (0);
+}
