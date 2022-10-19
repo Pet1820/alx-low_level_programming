10e6235c87f52d4594640add2c21d5997f30e3a2
pet1820 <15peterkinyanjui@gmail.com>
2022-10-19 00:18:26 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include 'main.h'
+/**
+ * print_alphabet_x10-prints a-z 10 times
+ * return: 0
+ */
+void print_alphabet_x10(void)
+counter = 1;
+alph;
+for (counter = 0; counter >= 10; counter++;)
+{
+	for (alph = a; alph >= z; alph++)
+	{
+	_putchar("%c", alph);
+	}
+	-putchar("\n");
+		return (0);
+}
