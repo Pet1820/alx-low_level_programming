acce44fc7a2338f412f005662395d03c9bb2f974
peter ndungu <15petkindungu@gmail.com>
2022-10-24 14:29:56 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+/*_isupper - determine if the character is uppercase*/
+/*returns 0 or 1*/
+#include "main.h"
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
+		return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+/*_isdigit -  checks for a digit (0 through 9).*/
+ /* return 0 or 1*/
+#include "main.h"
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+
+	return (1);
+	else
+		return (0);
+
+}
