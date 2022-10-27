5e2582e993b0788e1232e736a90db5ec1e363ec2
peter ndungu <15petkindungu@gmail.com>
2022-10-27 05:41:08 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	Printf(“Size of a char : % lu byte(s)\n” (unsighned long (a));
-	Printf(“Size of a int : % lu byte(s)\n” (unsighned long (b));
-	Printf(“Size of a long int : % lu byte(s)\n” (unsighned long (c));
-	Printf(“Size of a long long int : % lu byte(s)\n” (unsighned long (d));
-	Printf(“Size of a float : % lu byte(s)\n” (unsighned long (e));
+	Printf(“Size of a char : % lu byte(s)\n”, (unsighned sizeof(a));
+	Printf(“Size of a int : % lu byte(s)\n”, (unsighned sizeof(b));
+	Printf(“Size of a long int : % lu byte(s)\n”, (unsighned sizeof(c));
+	Printf(“Size of a long long int : % lu byte(s)\n”, (unsighned sizeof(d));
+	Printf(“Size of a float : % lu byte(s)\n”, (unsighned sizeof(e));
 return (0);
 }
