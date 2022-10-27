c8c0f5ea463b69f41a467ee884140b63d2e3ddbc
peter ndungu <15petkindungu@gmail.com>
2022-10-27 05:33:10 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 /**
- * prints the size of various types on the computer
- *it is compiled and run on
+ * main -Prints the size of various types on the computer
+ * It is compiled and run on
+ * Return: 0
 */
 int main(void)
 {
-       	char a:
-	int b:
-	long int c:
-	long long int d:
-	float e:
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	Printf(“Size of a char: %lu byte(s)\n” (unsighned long (a)); 
-	Printf(“Size of a int :%lu byte(s)\n” (unsighned long (b));
-	Printf(“Size of a long int :%lu byte(s)\n” (unsighned long (c));
-	Printf(“Size of a long long int :%lu byte(s)\n” (unsighned long (d));
-	Printf(“Size of a float :%lu byte(s)\n” (unsighned long (e));
-
+	Printf(“Size of a char : % lu byte(s)\n” (unsighned long (a));
+	Printf(“Size of a int : % lu byte(s)\n” (unsighned long (b));
+	Printf(“Size of a long int : % lu byte(s)\n” (unsighned long (c));
+	Printf(“Size of a long long int : % lu byte(s)\n” (unsighned long (d));
+	Printf(“Size of a float : % lu byte(s)\n” (unsighned long (e));
+return (0);
 }
-return (0)
-		
