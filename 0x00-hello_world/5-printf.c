20a4992f335e2cbedf5ae2e6c7972a9824982e9f
peter ndungu <15petkindungu@gmail.com>
2022-10-27 05:10:51 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-  *using printf 
-  *return 0
- */
+ * main -Print a line of character using printf
+ *
+ * Return: 0
+ *
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-return (0)
-
-
